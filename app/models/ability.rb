47cdcc5f8acdffd5313813de70acae121a633a41
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2017-02-03 23:03:38 -0300

    Forbid creating messages on closed tickets

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,19 +7,26 @@ class Ability
   def initialize(user)
 
     if user.type == 'Admin'
-      can [ :index, :show, :report ], Ticket
-      can [ :create ], Ticket::Message
       can [ :create ], Attendant
+      can [ :index, :show, :report ], Ticket
+      can [ :create ], Ticket::Message,
+        author_id: user.id,
+        ticket: { closed_at: nil }
     end
 
     if user.type == 'Attendant'
       can [ :index, :show, :report ], Ticket
-      can [ :create ], Ticket::Message
+      can [ :create ], Ticket::Message,
+        author_id: user.id,
+        ticket: { closed_at: nil }
     end
 
     if user.type == 'Customer'
       can [ :index, :show, :create ], Ticket, author_id: user.id
-      can [ :create ], Ticket::Message, author_id: user.id, ticket_id: user.tickets.pluck(:id)
+      can [ :create ], Ticket::Message,
+        author_id: user.id,
+        ticket_id: user.tickets.pluck(:id),
+        ticket: { closed_at: nil }
     end
 
   end
